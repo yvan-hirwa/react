8bdf05a9752a6fd55a4fdae0c2f3502521e81afd
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2026-01-22 00:51:41 +0200

    React props, array rendering

--- firstReactProject/src/App.jsx
+++ firstReactProject/src/App.jsx
@@ -1,19 +1,35 @@
 import './App.css'
 
 function App() {
+  const users = [
+    {
+      hobby: 'Football',
+      name: 'Alex',
+      age: 23
+    },
+    {
+      hobby: 'Cooking',
+      name: 'Eric',
+      age: 22
+    },
+    {
+      hobby: 'Coding',
+      name: 'Trevor',
+      age: 14
+    }
+  ]
   return (
     <div className="app-container">
       <h1>Hello React</h1>
-      
-      <Header />
+
+      {users.map(user => <Header hobby={user.hobby} name={user.name} age={user.age} />)}
     </div>
   )
 }
 
-function Header(){
-  const hobby = 'Playing football'
+function Header({ hobby, name, age }){
 
-  return <p>Hello, My hobby is: {hobby}</p>
+  return <p>Hello, My name is: {name} and my hobby is: {hobby}. I am {age} years old, I was born in {2026-age}</p>
 }
 
 export default App
