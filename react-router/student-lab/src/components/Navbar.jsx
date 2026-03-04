677a76ae1d6481871f7923b2b4a4acb1ebb88275
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2026-03-04 16:04:34 +0200

    add react router dom, change nav

--- react-router/student-lab/src/components/Navbar.jsx
+++ react-router/student-lab/src/components/Navbar.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="h-16 w-screen flex justify-center items-center space-x-3 text-indigo-500">
+      <Link to="/">Home</Link>
+      <Link to="/students">Students</Link>
+    </div>
+  );
 }
