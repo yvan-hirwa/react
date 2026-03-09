e94ec4ef21928fb9b3de11d41da74e34d18afb48
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2026-03-09 18:04:33 +0200

    Add NavLink

--- react-router/student-lab/src/pages/StudentsList.jsx
+++ react-router/student-lab/src/pages/StudentsList.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function StudentsList() {
   return (
     <div className="h-full w-xl flex flex-col justify-center items-start space-y-3">
-      <Link to="1">Student 1</Link>
-      <Link to="2">Student 2</Link>
-      <Link to="3">Student 3</Link>
-      <Link to="4">Student 4</Link>
-      <Link to="5">Student 5</Link>
+      <NavLink to="1">Student 1</NavLink>
+      <NavLink to="2">Student 2</NavLink>
+      <NavLink to="3">Student 3</NavLink>
+      <NavLink to="4">Student 4</NavLink>
+      <NavLink to="5">Student 5</NavLink>
     </div>
   );
 }
