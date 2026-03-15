71f86c88eb5df5bed910f8a665cf45ae66bc6a9a
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2026-03-16 00:13:17 +0200

    useReducer

--- react-exercises/rest/src/App.jsx
+++ react-exercises/rest/src/App.jsx
@@ -1,9 +1,11 @@
 // import { useEffect } from "react";
-import ContextApp from "./context1/ContextApp";
-import DarkMode from "./darkmode/DarkMode";
+// import ContextApp from "./context1/ContextApp";
+// import DarkMode from "./darkmode/DarkMode";
 // import Question4 from "./customHook/Question4";
 // import Question3 from "./fetchPost/Question3";
 // import Question2 from "./list-optimization/Question2";
+
+import PostLoader from "./useReducer/PostLoader";
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
       {/* <Question3 /> */}
       {/* <Question4 /> */}
       {/* <ContextApp /> */}
-      <DarkMode />
+      {/* <DarkMode /> */}
+      <PostLoader />
     </>
   );
 }
---
+++ react-exercises/rest/src/useReducer/PostLoader.jsx
@@ -0,0 +1,56 @@
+import { useCallback, useReducer } from "react";
+
+export default function PostLoader() {
+  function reducer(state, action) {
+    switch (action.type) {
+      case "FETCH_START":
+        return { ...state, loading: true, error: null };
+      case "FETCH_SUCCESS":
+        return { posts: action.payload, loading: false, error: null };
+      case "FETCH_ERROR":
+        return { ...state, loading: false, error: action.payload };
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, {
+    posts: [],
+    loading: false,
+    error: null,
+  });
+
+  const loadPosts = useCallback(async () => {
+    dispatch({ type: "FETCH_START" });
+    try {
+      const post = await fetch("https://jsonplaceholder.typicode.com/posts");
+      if (!post.ok) throw new Error("Posts didn't fetch");
+      const data = await post.json();
+      dispatch({ type: "FETCH_SUCCESS", payload: data });
+    } catch (error) {
+      dispatch({ type: "FETCH_ERROR", payload: error.message });
+    }
+  }, []);
+
+  return (
+    <div>
+      <button onClick={loadPosts} disabled={state.loading}>
+        Load Posts
+      </button>
+
+      <div>
+        {state.loading ? (
+          <p>Loading...</p>
+        ) : state.error ? (
+          <p>{state.error}</p>
+        ) : (
+          <ul>
+            {state.posts.map((post) => (
+              <li key={post.id}>{post.title}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
