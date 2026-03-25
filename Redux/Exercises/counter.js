2c3d8e1bcb3579dae9565893414f6632a37719cb
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2026-03-25 16:32:41 +0200

    done: vanilla Redux state management

--- Redux/Exercises/counter.js
+++ Redux/Exercises/counter.js
@@ -10,3 +10,33 @@ function counterReducer(state = initialState, action) {
       return state;
   }
 }
+
+function createStore(reducer) {
+  let state;
+  const listeners = [];
+
+  state = reducer(state, { type: "", payload: 0 });
+
+  return {
+    getState: () => state,
+    dispatch: (action) => {
+      state = reducer(state, action);
+      listeners.forEach((listener) => listener(state));
+      return;
+    },
+    subscribe: (listener) => listeners.push(listener),
+  };
+}
+
+function logState(state) {
+  console.log(state);
+}
+const store = createStore(counterReducer);
+store.subscribe(logState);
+
+store.dispatch({ type: "INCREMENT", payload: 8 });
+store.dispatch({ type: "DECREMENT", payload: 6 });
+store.dispatch({ type: "INCREMENT", payload: 3 });
+store.dispatch({ type: "DECREMENT", payload: 5 });
+store.dispatch({ type: "INCREMENT", payload: 8 });
+store.dispatch({ type: "DECREMENT", payload: 2 });
